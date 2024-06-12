bc1e9d9a23d0f203f076c0faeeedb8dceb3e1dd1
VRinSat <vrinda.satish25@gmail.com>
2024-06-12 12:01:54 +0300

    code sur python

---
+++ main.py
@@ -0,0 +1,11 @@
+if __name__ == "__main__":
+    a=int(input("Donner valeur de a: "))
+    b=int(input("Donner valeur de b: "))
+    if (a+b) == 9:
+        print("a+b=9")
+    elif (a+b)>9:
+        print("a+b est supérieur à 9")
+    elif (a+b)<9:
+        print("a+b est inférieur à 9")    
+    else :
+        print("a+b n'est pas égale à 9")
